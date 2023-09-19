2ca0e94a86576d371a9545db2cb4ebd125b68ad6
aizhigito <82809523+aizhigito@users.noreply.github.com>
2023-09-20 08:52:08 +0900

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mini-fastapi-gateway',
-    version='0.0.1-beta',
+    version='0.0.1-beta-1',
     packages=find_packages(),
     url='',
     license='',
