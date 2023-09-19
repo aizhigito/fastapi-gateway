cf4a56f31e66cb81e1cd00c27409a649d9bf0229
aizhigito <82809523+aizhigito@users.noreply.github.com>
2023-09-20 08:30:42 +0900

    fix setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='fastapi-gateway',
+    name='mini-fastapi-gateway',
     version='0.0.1',
     packages=find_packages(),
     url='',
