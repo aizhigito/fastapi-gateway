f8a40f29573d900789982a17ee0a1c144d19816c
aizhigito <82809523+aizhigito@users.noreply.github.com>
2023-09-20 10:46:00 +0900

    db_url default to sqlite

--- gateway/server/core/config.py
+++ gateway/server/core/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    db_url: str = Field(..., env="GATEWAY_DB_URL")
+    db_url: str = Field(default="sqlite:///sql_app.db", env="GATEWAY_DB_URL")
 
 
 settings = Settings()
