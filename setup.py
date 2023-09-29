f846f1bdc6dbb45a33164e1fe1cc4e5f2cb98642
aizhigito <82809523+aizhigito@users.noreply.github.com>
2023-09-29 15:20:37 +0900

    0.0.2-beta-7

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='mini-fastapi-gateway',
-    version='0.0.2-beta-6',
+    version='0.0.2-beta-7',
     packages=find_packages(),
     url='',
     license='',
     author='aizhigito',
     author_email='aizhigit94@gmail.com',
     description='FastAPI gateway',
-    download_url='https://github.com/aizhigito/fastapi-gateway/archive/refs/tags/v0.0.2-beta-6.tar.gz',
+    download_url='https://github.com/aizhigito/fastapi-gateway/archive/refs/tags/v0.0.2-beta-7.tar.gz',
     install_requires=[
         'fastapi',
         'sqlalchemy',
